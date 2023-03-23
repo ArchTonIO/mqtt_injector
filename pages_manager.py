42ad536242dbebda368b52dec43a6d19d9770fe1
ArchTonIO <delchi1199@gmail.com>
2023-03-23 03:00:47 +0100

    added 'destroy_last_page' method

--- pages_manager.py
+++ pages_manager.py
@@ -1,21 +1,10 @@
 """
 This module is used to manage the pages of the menu.
 """
-# pylint: disable=import-error
-# pylint: disable=no-name-in-module
 
-# from time import sleep
 import json
 from page import Page
-# from commands import CommandExecuter
-# from hardware_manager import HwMan
-
-# ACTUAL_ENCODER_VALUE = 0
-# OLED = HwMan.oled
-# ENCODER = HwMan.encoder
-# SEL_BUTTON = HwMan.select_button
-# OFF_POS = 6
-# cmd_executer = CommandExecuter()
+from data_page import DataPage
 
 
 class PMan():
@@ -34,7 +23,7 @@ class PMan():
             entries = [
                 entry for entry in menu[key].keys() if "__" not in entry
             ]
-            PMan.__add_page(
+            PMan.add_page(
                 page_uid=menu[key]["__page_uid"],
                 entries=entries,
                 parent=menu[key]["__parent"],
@@ -44,10 +33,11 @@ class PMan():
                 cursor_default_position=menu[key]["__cursor_default_position"],
             )
             PMan.back_positions.append((menu[key]["__page_uid"], len(entries)))
+            PMan.target_page = PMan.pages["A0"]
         print("done")
 
     @staticmethod
-    def __add_page(
+    def add_page(
         page_uid: int,
         entries: list,
         parent: str,
@@ -91,105 +81,13 @@ class PMan():
                 ]
             ]
 
+    @staticmethod
+    def destroy_last_page() -> None:
+        """
+        This method is used to destroy the last page of the menu.
+        """
+        PMan.pages.pop(-1)
+        PMan.back_positions.pop(-1)
+
 
 PMan.build_menu_from_json(json_file="settings/pages.json")
-PMan.target_page = PMan.pages["A0"]
-
-#     def destroy_last_page(self) -> None:
-#         """
-#         This method is used to destroy the last page of the menu.
-#         - Args:
-#             - None.
-#         - Returns:
-#             - None.
-#         """
-#         self.pages.pop(-1)
-#         self.back_positions.pop(-1)
-
-#     def __display_page(self, spacing=11) -> None:
-#         """
-#         This method is used to display the current page.
-#         - Args:
-#             - spacing: the vertical spacing between
-#                 the lines (in pixels), default is 11.
-#         - Returns:
-#             - None.
-#         """
-#         self.oled.fill(0)
-#         line_increment = 0
-#         if self.actual_encoder_value > 5:
-#             line_increment = self.actual_encoder_value-5
-#         display_lines = (
-#             self.target_page.page_lines[line_increment:line_increment+6]
-#         )
-#         raw_counter = 0
-#         for line in display_lines:
-#             self.oled.text(line, 0, raw_counter)
-#             raw_counter += spacing
-#         self.oled.show()
-
-#     def __action(self) -> None:
-#         """
-#         This method is called whenever the select button is pressed.
-#         Its work is to execute the command associated with the
-#         current page and the current cursor position.
-#         - Args:
-#             - None.
-#         - Returns:
-#             - None.
-#         """
-#         if (self.target_page == self.pages[0]
-#                 and self.actual_encoder_value == self.off_pos):
-#             self.oled.fill(0)
-#             self.oled.show()
-#         elif ((self.target_page.id,
-#                 self.actual_encoder_value) in self.back_positions
-#                 or not self.target_page.is_leaf):
-#             self.__change_page()
-#         else:
-#             self.cmd_executer.execute(
-#                 self,
-#                 self.target_page.id,
-#                 self.actual_encoder_value
-#             )
-
-#     def __change_page(self) -> None:
-#         """
-#         This method is used to change the current page
-#         to the page associated with the current cursor position.
-#         - Args:
-#             - None.
-#         - Returns:
-#             - None.
-#         """
-#         if self.target_page == self.pages[0]:
-#             self.target_page = self.pages[self.actual_encoder_value+1]
-#         elif ((self.target_page.id, self.actual_encoder_value)
-#                 in self.back_positions):
-#             self.target_page = self.pages[0]
-#         self.target_page.build_page()
-#         self.__display_page()
-
-#     def loop(self) -> None:
-#         """
-#         This method is used to start the menu loop,
-#         this is necessary to make the menu work.
-#         - Args:
-#             - None.
-#         - Returns:
-#             - None.
-#         """
-#         self.target_page = self.pages[0]
-#         self.target_page.build_page()
-#         self.__display_page()
-#         while True:
-#             last_encoder_value = self.rotary_encoder.value()
-#             sleep(0.15)
-#             print(last_encoder_value)
-#             if self.actual_encoder_value != last_encoder_value:
-#                 self.actual_encoder_value = last_encoder_value
-#                 self.target_page.cursor_position = self.actual_encoder_value
-#                 self.target_page.build_page()
-#                 self.__display_page()
-#             if not self.select_button.value():
-#                 self.__action()
